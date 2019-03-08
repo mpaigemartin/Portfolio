d238db5f2f3163ab78afdb59427f14ab28ff40f5
Paige <mpaigemartin@gmail.com>
2019-03-08 13:33:52 -0500

    finally fixed mod modals and stuffff

--- main.js
+++ main.js
@@ -18,34 +18,5 @@ $(function() {
   $("#submit").on("click", contact);
 
   $(".bar").on("click", openNav);
-
-  // $("#myBtn").click(function() {
-  //   console.log("workign");
-  //   $("#myModal").modal();
-  // });
-
-  $("#myBtn").click(function() {
-    $("#myModal").modal();
-  });
-
-  // const modal = document.getElementById("myModal");
-  // const btn = document.getElementById("myBtn");
-  const span = document.getElementsByClassName("close")[0];
-
-  // console.log(btn);
-
-  // btn.click = function() {
-  //   console.log("blegh");
-  //   modal.style.display = "block";
-  // };
-
-  // span.onclick = function() {
-  //   modal.style.display = "none";
-  // };
-
-  // window.onclick = function(event) {
-  //   if (event.target == modal) {
-  //     modal.style.display = "none";
-  //   }
-  // };
+  $(".closebtn").on("click", closeNav);
 });
